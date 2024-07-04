6c6187110dc37fa9935be86dd24dcbe20cd0bc94
MeowB <60074375+MeowB@users.noreply.github.com>
2024-07-04 15:59:56 +0200

    finished mobile layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,77 @@
-import { useState } from 'react'
-import './App.css'
-import Form from './components/Form'
+import { useState } from 'react';
+import './App.css';
+import Result from './components/Result';
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [amount, setAmount] = useState(0)
+	const [term, setTerm] = useState(0)
+	const [rate, setRate] = useState(0)
+	const [type, setType] = useState('')
+
+	function handleSubmit() {
+		handleRadioChange()
+	}
+
+	function handleAmountChange() {
+		const value = document.getElementById('mortgage-amount').value
+		setAmount(value)
+	}
+
+	function handleTermChange() {
+		const value = document.getElementById('mortgage-term').value
+		setTerm(value)
+	}
+
+	function handleInterestChange() {
+		const value = document.getElementById('interest-rate').value
+		setRate(value)
+	}
+
+	function handleRadioChange() {
+		const value = document.querySelector('input[name=mortgage-type]:checked').value
+		setType(value)
+	}
+
 
   return (
     <>
 		<div className="main-content">
 			<h1>Mortgage Calculator</h1>
 			<a href="#">Clear All</a>
-			<Form />
-		</div>
-		<div className="result">
+			<div className="form">
+				<form action="#">
+					<div className="mortgage-amount">
+						<label htmlFor="mortgage-amount">Mortgage Amount</label>
+						<input type="number" name="mortgage-amount" id="mortgage-amount" onChange={handleAmountChange} />
+					</div>
+		
+					<div className="mortgage-term">
+						<label htmlFor="mortgage-term">Mortgage Term</label>
+						<input type="number" name="mortgage-term" id="mortgage-term" onChange={handleTermChange} />
+						<p>years</p>
+					</div>	
+					
+					<label htmlFor="interest-rate">Interest Rate</label>
+					<input type="number" name="interest-rate" id="interest-rate" onChange={handleInterestChange} />
 
+					
+
+					<div className="radio">
+						<h2>Mortgage Type</h2>
+						<div>
+							<label for="html">Repayement</label><br></br>
+							<input type="radio" id="repayement" name="mortgage-type" value="repayement" />
+						</div>
+						<div>
+							<label for="html">Interest Only</label><br></br>
+							<input type="radio" id="interest" name="mortgage-type" value="interest-only" />
+						</div>
+					</div>
+
+					<input type="button" onClick={handleSubmit} />
+				</form>
+			</div>
+			<Result amount={amount} term={term} rate={rate} type={type}/>
 		</div>
     </>
   )
---
+++ src/components/Result.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import './result.css'
+import illustrationSvg from '../assets/images/illustration-empty.svg'
+
+function Result(props) {
+	const bool = true
+
+	if (bool) {
+		return (
+			<div className="result-filled">
+				<div className="text-content">
+					<h2>Your results</h2>
+					<p>Your results are shown below based on the information you provided. To adjust the results, edit the form and click “calculate repayments” again.</p>
+				</div>
+				<div className="overview">
+					<div className="monthly">
+						<p>Your monthly repayments</p>
+						<span>1.222.15</span>
+					</div>
+					<div className="total">
+						<p>Total you'll repay over the term</p>
+						<span>555.555.22</span>
+					</div>
+				</div>
+			</div>
+		)
+	} else {
+		return (
+			<div className="result-empty">
+				<img src={illustrationSvg} alt="illustration" />
+				<h2>Results shown here</h2>
+				<p>Complete the form and click "calculate repayments" to see what your monthly repayments would be</p>
+			</div>
+		)
+	}
+
+
+}
+
+
+export default Result
